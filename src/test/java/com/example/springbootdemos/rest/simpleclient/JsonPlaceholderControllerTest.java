b9d58cc0a33c3629c862f25f1e60f8a291383e1c
Angel Aguado <angeagu@gmail.com>
2020-04-21 22:43:40 +0200

    Added Rest Doc and AsciiDoc Documentation.

--- src/test/java/com/example/springbootdemos/rest/simpleclient/JsonPlaceholderControllerTest.java
+++ src/test/java/com/example/springbootdemos/rest/simpleclient/JsonPlaceholderControllerTest.java
@@ -1,17 +1,22 @@
 package com.example.springbootdemos.rest.simpleclient;
 
 import static org.mockito.Mockito.mock;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.example.springbootdemos.rest.shared.BaseControllerTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import static org.mockito.Mockito.when;
 
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+@AutoConfigureRestDocs(outputDir = "target/snippets")
 public class JsonPlaceholderControllerTest extends BaseControllerTest {
 
 	@MockBean
@@ -29,7 +34,14 @@ public class JsonPlaceholderControllerTest extends BaseControllerTest {
     	this.mockMvc.perform(get("/demo/rest/consumer/jsonplaceholder/todo/22")
     			.accept(MediaType.APPLICATION_JSON_VALUE))
     			.andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(MockMvcRestDocumentation.document("jsonplaceholder/todo",
+						responseFields(
+								fieldWithPath("completed").description("Todo Status (true=completed)"),
+								fieldWithPath("id").description("Todo ID"),
+								fieldWithPath("title").description("Todo Title"),
+								fieldWithPath("userId").description("Todo User ID")
+						)));
     }
     
 }
